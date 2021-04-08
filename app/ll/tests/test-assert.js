8d6b8320bb1bf3baa3d138e0a3b8f2be6d979842
leikareipa <drilledicecream@gmail.com>
2021-04-08 05:57:20 +0300

    test/assert: Add async expecters

--- app/ll/tests/test-assert.js
+++ app/ll/tests/test-assert.js
@@ -13,7 +13,9 @@ const {LLTest_Error,
 
 module.exports = {
     LLTest_ExpectTrue: expect_true,
+    LLTest_ExpectTrueAsync: expect_true_async,
     LLTest_ExpectThrow: expect_throw,
+    LLTest_ExpectThrowAsync: expect_throw_async,
 };
 
 function expect_true(test_fn = ()=>false)
@@ -22,15 +24,35 @@ function expect_true(test_fn = ()=>false)
 
     try
     {
-        if (!test_fn())
-        {
+        if (!test_fn()) {
             throw LLTest_Error(`Not true: ${test_fn.toString()}`);
         }
     }
     catch (error)
     {
-        if (!LLTest_IsOwnError(error))
-        {
+        if (!LLTest_IsOwnError(error)) {
+            console.error("Encountered a runtime error while evaluating test expression.");
+        }
+
+        throw error;
+    }
+
+    return;
+}
+
+async function expect_true_async(async_test_fn = async()=>false)
+{
+    LL_Assert((typeof async_test_fn === "function"), "Invalid arguments.");
+
+    try
+    {
+        if (!(await async_test_fn())) {
+            throw LLTest_Error(`Not true: ${async_test_fn.toString()}`);
+        }
+    }
+    catch (error)
+    {
+        if (!LLTest_IsOwnError(error)) {
             console.error("Encountered a runtime error while evaluating test expression.");
         }
 
@@ -54,4 +76,20 @@ function expect_throw(test_fn = ()=>{throw 0})
     }
     
     throw LLTest_Error(`No throw: ${test_fn.toString()}`);
+}
+
+async function expect_throw_async(async_test_fn = async()=>{throw 0})
+{
+    LL_Assert((typeof async_test_fn === "function"), "Invalid arguments.");
+
+    try
+    {
+        await async_test_fn();
+    }
+    catch
+    {
+        return;
+    }
+    
+    throw LLTest_Error(`No throw: ${async_test_fn.toString()}`);
 }
